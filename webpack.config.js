e90de398ac29072c3d25a8ffd22cc1cd64402d83
恩田雅大 <masa@masa-2.local>
2019-01-05 12:59:51 +0900

    update package.json, package-lock.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,5 +15,28 @@ module.exports = {
       filename: '[name].js',
       library: ["com", "example"],
       libraryTarget: 'umd'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    "useBuiltIns": "usage",
+                                    "targets": "> 0.25%, not dead"
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'inline-source-map'
 };
